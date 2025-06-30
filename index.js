4a210ff0225c77c93a0c8b62b2e15d58df2e9793
salamantalel <nmoinkett@gmail.com>
2025-06-30 16:16:23 +0300

    Complete dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+ // Remove the <main id="main"> element from the page
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new <h1> to 'victory'
+newHeader.id = 'victory';
+
+// Set the text inside the <h1> element
+newHeader.textContent = "Sal is the champion";
+
+// Append the new <h1> to the body
+document.body.append(newHeader);
